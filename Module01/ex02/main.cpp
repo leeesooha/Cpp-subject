8af5c2f43361d7d3ca956afea58f6a4c71d46777
leeesooha <suha630@naver.com>
2023-10-02 10:51:51 +0900

    ex02 Ready to Submit

--- Module01/ex02/main.cpp
+++ Module01/ex02/main.cpp
@@ -3,15 +3,15 @@
 
 int main(void)
 {
-	std::string str = "HI THIS IS BRAIN";
-	std::string *stringPTR = &str;
-	std::string &stringREF = str;
+	std::string A = "HI THIS IS BRAIN";
+	std::string *stringPTR = &A;
+	std::string &stringREF = A;
 
-	std::cout << &str << std::endl;
+	std::cout << &A << std::endl;
 	std::cout << stringPTR << std::endl;
 	std::cout << &stringREF << std::endl;
 
-	std::cout << str << std::endl;
+	std::cout << A << std::endl;
 	std::cout << *stringPTR << std::endl;
 	std::cout << stringREF << std::endl;
 	return (0);
