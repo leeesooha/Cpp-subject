0da1a100263b16cd1546aac7c96b245a3d92dd3f
leeesooha <suha630@naver.com>
2023-10-02 04:26:33 +0000

    ex01 remove to_string(c++11)

--- Module01/ex01/zombieHorde.cpp
+++ Module01/ex01/zombieHorde.cpp
@@ -1,14 +1,20 @@
 #include "Zombie.hpp"
+#include <sstream>
 
 Zombie* zombieHorde( int N, std::string name )
 {
 	if (N < 1)
 		return (NULL);
 	Zombie *zombies = new Zombie[N];
+	std::ostringstream ss;
+	std::string str;
 
 	for (int i = 0; i < N; i++)
 	{
-		zombies[i].setZombie(name + "[" + std::to_string(i) + "]");
+		ss.str("");
+    	ss << i;
+    	str = ss.str();
+		zombies[i].setZombie(name + "[" + str + "]");
 	}
 	return zombies;
 }
