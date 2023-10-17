e40f7383d90166c50423b0b194fcb12de5ff6352
leeesooha <suha630@naver.com>
2023-10-17 09:20:10 +0900

    add Makefile,main.cpp,ClapTrap.hpp,cpp in cpp Module03

---
+++ Module03/ex00/ClapTrap.cpp
@@ -0,0 +1,40 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+
+}
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+
+}
+
+ClapTrap::~ClapTrap()
+{
+
+}
+
+void attack(const std::string& target)
+{
+
+}
+
+void takeDamage(unsigned int amount)
+{
+
+}
+
+void beRepaired(unsigned int amount)
+{
+
+}
