72f5ebbf1944515f3299244c8e895f95f63f56f7
leeesooha <suha630@naver.com>
2023-10-23 19:41:19 +0900

    Module ex02 complete

--- Module04/ex02/Animal.hpp
+++ Module04/ex02/Animal.hpp
@@ -13,7 +13,7 @@ class Animal
         Animal& operator=(const Animal& other);
         virtual ~Animal();
         virtual std::string getType() const;
-        virtual void makeSound() const;
+        virtual void makeSound() const = 0;
     protected :
         std::string _type;
 };
