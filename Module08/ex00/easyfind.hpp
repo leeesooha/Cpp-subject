a012f934e7e505a40564af178c6ada3be71331ea
leeesooha <suha630@naver.com>
2023-12-01 18:55:56 +0900

    asdf

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -1,10 +1,18 @@
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
-template <typename T>
-void easyfind(T &a, int target)
-{
+#include <algorithm>
 
+template <typename T>
+T& easyfind(T &container, int target) const
+{
+	T* ptr;
+
+	ptr = std::find(container.begein(), container.last(), target);
+	if (ptr == container.last())
+		throw std::out_of_range("not found value!");
+	else
+		return (ptr);
 }
 
 #endif
